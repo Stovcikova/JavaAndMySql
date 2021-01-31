4f2e5d344dbebd7f6678627b630eec9a6af5ed6a
Stovcikova <henrieta.stovcikova@kosickaakademia.sk>
2021-01-31 21:26:10 +0100

    first commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+package sk.kosickaakademmia.stovcikova.mysql;
+
+public class Main {
+    public static void main(String[] args) {
+        Database database = new Database();
+        database.showCities("Argentina");
+    }
+}
